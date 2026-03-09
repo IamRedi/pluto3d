47fb6d8022c11b447735bb7a1fca2ca30dc08e97
IamRedi <redioncekrezi@gmail.com>
2026-03-09 22:00:12 +0100

    simpler svg generator

--- backend/app/routes/svg.py
+++ backend/app/routes/svg.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, UploadFile, File
 import uuid
 import os
-import vtracer
+import cv2
 
 router = APIRouter()
 
@@ -10,7 +10,6 @@ SVG_DIR = "outputs/svg"
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 os.makedirs(SVG_DIR, exist_ok=True)
-
 
 @router.post("/svg")
 async def generate_svg(file: UploadFile = File(...)):
@@ -23,10 +22,18 @@ async def generate_svg(file: UploadFile = File(...)):
     with open(input_path, "wb") as f:
         f.write(await file.read())
 
-    vtracer.convert_image_to_svg(
-        input_path,
-        output_path
-    )
+    img = cv2.imread(input_path, cv2.IMREAD_GRAYSCALE)
+    edges = cv2.Canny(img,100,200)
+
+    h, w = edges.shape
+
+    with open(output_path, "w") as svg:
+        svg.write(f'<svg xmlns="http://www.w3.org/2000/svg" width="{w}" height="{h}">')
+        for y in range(h):
+            for x in range(w):
+                if edges[y][x] > 0:
+                    svg.write(f'<rect x="{x}" y="{y}" width="1" height="1" fill="black"/>')
+        svg.write('</svg>')
 
     return {
         "svg_url": f"/outputs/svg/{file_id}.svg"
