9d507c83f102f319deb8a73cace352e9ff18dbad
IamRedi <redioncekrezi@gmail.com>
2026-03-14 15:45:53 +0100

    fix blueprint engine indentation

--- backend/app/services/blueprint_engine.py
+++ backend/app/services/blueprint_engine.py
@@ -3,10 +3,12 @@ import svgwrite
 import uuid
 import os
 
-#from app.services.object_detector import detect_main_object
+# from app.services.object_detector import detect_main_object
 from app.services.crop_object import crop_object
 
 OUTPUT_DIR = "outputs"
+
+
 def generate_blueprint(image_path):
 
     img = cv2.imread(image_path)
@@ -15,16 +17,12 @@ def generate_blueprint(image_path):
 
     boxes = []
 
-    # ---------------- OBJECT DETECTION ----------------
-
-boxes = []
-
-if len(boxes) > 0:
-    box = boxes[0]
-    print("Object detected → cropping")
-    img = crop_object(img, box)
-else:
-    print("No object detected → using full image")
+    if len(boxes) > 0:
+        box = boxes[0]
+        print("Object detected → cropping")
+        img = crop_object(img, box)
+    else:
+        print("No object detected → using full image")
 
     # ---------------- EDGE PREPROCESS ----------------
 
@@ -34,7 +32,7 @@ else:
     gray = cv2.bilateralFilter(gray, 9, 75, 75)
 
     # blur i lehtë
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
     # edges më të qëndrueshme
     edges = cv2.Canny(blur, 40, 120)
@@ -43,29 +41,27 @@ else:
 
     contours, _ = cv2.findContours(
         edges,
-        cv2.RETR_EXTERNAL,   # vetëm konturet kryesore
+        cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_NONE
     )
 
     h, w = edges.shape[:2]
 
     name = f"{uuid.uuid4()}.svg"
-
     output = os.path.join(OUTPUT_DIR, name)
 
     dwg = svgwrite.Drawing(output, size=(w, h))
 
     for cnt in contours:
 
-        # filtrimi i noise shumë të vogël
         area = cv2.contourArea(cnt)
 
-        if area <5:
+        # filtro noise shumë të vogël
+        if area < 5:
             continue
 
         # smooth contour
         epsilon = 0.002 * cv2.arcLength(cnt, True)
-
         approx = cv2.approxPolyDP(cnt, epsilon, True)
 
         points = [(int(p[0][0]), int(p[0][1])) for p in approx]
